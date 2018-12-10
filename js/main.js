95c73bf9633123bfd5539a75ed3ec2f58c73f6da
srikanth Karra <srikanthreddy376@gmail.com>
2018-12-10 19:56:36 +0100

    added some stying

--- js/main.js
+++ js/main.js
@@ -10,5 +10,46 @@ $("#side-menu-div li.close").click(function(){
      $('#side-menu-div i').fadeIn(200).css('display','block');
 });
 
+// smooth scrrolling 
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+ 
+      if (target.length) {
+      
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1250, function() {
+        
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { 
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); 
+            $target.focus(); 
+          };
+        });
+      }
+    }
+  });
+//animate css
 
 
+$(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() >1200 ) {
+      $('#about p').addClass('animated fadeIn');
+      
+   }
+});
